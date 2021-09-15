e96ee2f976e1e5469b528eed1d2e320e6348db8e
Kumar Khadka <kumarbahadurkhadka11@gmail.com>
2021-09-15 08:50:45 +0545

    training_session

---
+++ index.js
@@ -0,0 +1,32 @@
+//let a = 1; //Number
+//let b = "hello world"; //string
+
+//console.log(typeof a);
+
+//let iims= null;
+//console.log(iims);
+
+let person ={
+    name:'kumar',
+    age: 22,
+    address:'kirtipur',
+}
+console.log("i am ",person.name);
+
+let exampleArray = Array (1, 2, 3, 4, 5, 6, 7);
+let exampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8,9,10];
+console.log(exampleArray);
+console.log(exampleArray2);
+
+
+
+
+
+
+
+
+
+
+
+
+9
